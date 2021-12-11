2fac22c326b58ec6090bef5418bd84996bf15cb4
AfzalKamboh <87604369+AfzalKamboh@users.noreply.github.com>
2021-12-11 15:55:33 +0500

    Image Guessing Game
    This is Image Guessing Game Project Language is C#.used the windows form application.

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace WorkingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnpic = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn15 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn16 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnpic
+            // 
+            this.btnpic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnpic.Location = new System.Drawing.Point(13, 13);
+            this.btnpic.Name = "btnpic";
+            this.btnpic.Size = new System.Drawing.Size(114, 101);
+            this.btnpic.TabIndex = 0;
+            this.btnpic.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(13, 120);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(114, 101);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(13, 334);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(114, 101);
+            this.btn9.TabIndex = 3;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(13, 227);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(114, 101);
+            this.btn5.TabIndex = 2;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn13
+            // 
+            this.btn13.Location = new System.Drawing.Point(12, 441);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(114, 101);
+            this.btn13.TabIndex = 4;
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn14
+            // 
+            this.btn14.Location = new System.Drawing.Point(132, 441);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(114, 101);
+            this.btn14.TabIndex = 8;
+            this.btn14.UseVisualStyleBackColor = true;
+            this.btn14.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(133, 334);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(114, 101);
+            this.btn10.TabIndex = 7;
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(133, 227);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(114, 101);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(133, 120);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(114, 101);
+            this.btn2.TabIndex = 5;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn15
+            // 
+            this.btn15.Location = new System.Drawing.Point(252, 441);
+            this.btn15.Name = "btn15";
+            this.btn15.Size = new System.Drawing.Size(114, 101);
+            this.btn15.TabIndex = 12;
+            this.btn15.UseVisualStyleBackColor = true;
+            this.btn15.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(253, 334);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(114, 101);
+            this.btn11.TabIndex = 11;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(253, 227);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(114, 101);
+            this.btn7.TabIndex = 10;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(253, 120);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(114, 101);
+            this.btn3.TabIndex = 9;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn16
+            // 
+            this.btn16.Location = new System.Drawing.Point(372, 441);
+            this.btn16.Name = "btn16";
+            this.btn16.Size = new System.Drawing.Size(114, 101);
+            this.btn16.TabIndex = 16;
+            this.btn16.UseVisualStyleBackColor = true;
+            this.btn16.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(373, 334);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(114, 101);
+            this.btn12.TabIndex = 15;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(373, 227);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(114, 101);
+            this.btn8.TabIndex = 14;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.click_button);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(373, 120);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(114, 101);
+            this.btn4.TabIndex = 13;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.click_button);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(134, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(355, 101);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Guess Picture Three Times From Puzzle (Attempts Allowed = 3)\r\n\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(501, 550);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn16);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn15);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn14);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btnpic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.click_button);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnpic;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn15;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn16;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkingGame
+{
+    public partial class Form1 : Form
+    {
+        int increment = 0;
+        int firstpicnumber, spicnumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Random rand=new Random();
+            int num = rand.Next(1, 17);
+            btnpic.Image = Image.FromFile(@"E:\Afzal's Data\Local Disk\5th_Semester\Visual_Programming( C Sharp )\Images\candy" + num + ".jpg");
+            btnpic.ImageAlign = ContentAlignment.MiddleRight;
+            firstpicnumber = num;
+            btnpic.FlatStyle = FlatStyle.Flat;
+        }
+        int counter = 0;
+        private void click_button(object sender, EventArgs e)
+        {
+            if(increment < 3)
+            {
+                Random random = new Random();
+                Button b = (Button)sender;
+                int num = random.Next(1, 17);
+                b.Image = Image.FromFile(@"E:\Afzal's Data\Local Disk\5th_Semester\Visual_Programming( C Sharp )\Images\candy" + num + ".jpg");
+                spicnumber = num;
+                b.ImageAlign = ContentAlignment.MiddleRight;
+                b.FlatStyle = FlatStyle.Flat;
+                increment += 1;
+                if(firstpicnumber == spicnumber)
+                {
+                    counter ++;
+                    if(counter==3)
+                    {
+                        MessageBox.Show("You Won");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Out of Limit ");
+                MessageBox.Show("You Lost");
+               
+            }
+        }
+    }
+}
